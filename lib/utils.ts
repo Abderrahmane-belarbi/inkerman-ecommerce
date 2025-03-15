6c7e22f946324cbf8eb653e396dc4f5e08b5cd3f
abderrahmane <abdoustart007@gmail.com>
2025-03-15 12:55:11 -0700

    create todays deals slider - products with title and ratings and price and deal off

--- lib/utils.ts
+++ lib/utils.ts
@@ -19,3 +19,16 @@ export function toSlug(text: string) {
     .replace(/^-+|-+$/g, "");
 }
 
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+  currency: "USD",
+  style: "currency",
+  minimumFractionDigits: 2,
+});
+export function formatCurrency(amount: number) {
+  return CURRENCY_FORMATTER.format(amount);
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number);
+}
