619ee92b74b22dd9804ffad214a4eda8b0093914
abderrahmane <abdoustart007@gmail.com>
2025-03-15 14:59:51 -0700

    create product page details

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,8 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Inkerman'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Inkerman";
 export const APP_SLOGAN =
-  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, save more'
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, save more";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  'E-commerce website for the best products. We have a wide selection of the best products in the world. From clothing to electronics, we have it all. Shop now and get the best products for the best price.'
+  "E-commerce website for the best products. We have a wide selection of the best products in the world. From clothing to electronics, we have it all. Shop now and get the best products for the best price.";
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
