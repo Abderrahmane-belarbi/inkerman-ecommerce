0ccfed782f676eb3c1f9c7ab5c2e589b6dc21682
abderrahmane <abdoustart007@gmail.com>
2025-03-18 18:10:12 -0700

    creating signin with google

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -12,6 +12,10 @@ export async function signInWithCredentials(user: IUserSignIn) {
   return await signIn("credentials", { ...user, redirect: false });
   // we dont want to redirect user automatically after signin, we want to do it manually from the form
 }
+
+export const SignInWithGoogle = async () => {
+  await signIn('google')
+}
 export const SignOut = async () => {
   const redirectTo = await signOut({ redirect: false });
   redirect(redirectTo.redirect);
