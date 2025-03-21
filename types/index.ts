e55389dd1bd3c3cfe316316be6446e7f0ed522af
abderrahmane <abdoustart007@gmail.com>
2025-03-20 17:25:34 -0700

    adding revies functionality

--- types/index.ts
+++ types/index.ts
@@ -4,16 +4,31 @@ import {
   OrderInputSchema,
   OrderItemSchema,
   ProductInputSchema,
+  ReviewInputSchema,
   ShippingAddressSchema,
   UserInputSchema,
   UserSignInSchema,
   UserSignUpSchema,
-} from '@/lib/validator'
+} from "@/lib/validator";
 
+export type IReviewInput = z.infer<typeof ReviewInputSchema>;
+export type IReviewDetails = IReviewInput & {
+  _id: string;
+  createdAt: string;
+  user: {
+    name: string;
+  };
+};
 export type IProductInput = z.infer<typeof ProductInputSchema>;
+
 export type Data = {
   users: IUserInput[];
   products: IProductInput[];
+  reviews: {
+    title: string;
+    rating: number;
+    comment: string;
+  }[];
   headerMenus: {
     name: string;
     href: string;
@@ -27,13 +42,12 @@ export type Data = {
   }[];
 };
 
-
-export type OrderItem = z.infer<typeof OrderItemSchema>
-export type IOrderInput = z.infer<typeof OrderInputSchema>
-export type Cart = z.infer<typeof CartSchema>
-export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
+export type OrderItem = z.infer<typeof OrderItemSchema>;
+export type IOrderInput = z.infer<typeof OrderInputSchema>;
+export type Cart = z.infer<typeof CartSchema>;
+export type ShippingAddress = z.infer<typeof ShippingAddressSchema>;
 
 // User
-export type IUserInput = z.infer<typeof UserInputSchema>
-export type IUserSignIn = z.infer<typeof UserSignInSchema>
-export type IUserSignUp = z.infer<typeof UserSignUpSchema>
+export type IUserInput = z.infer<typeof UserInputSchema>;
+export type IUserSignIn = z.infer<typeof UserSignInSchema>;
+export type IUserSignUp = z.infer<typeof UserSignUpSchema>;
