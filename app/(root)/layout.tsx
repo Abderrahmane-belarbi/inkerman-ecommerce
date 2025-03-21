8427c0212cf6efe995620a6b529a2f35b57be186
abderrahmane <abdoustart007@gmail.com>
2025-03-21 06:45:53 -0700

    creating category sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,7 +11,9 @@ export default async function RootLayout({
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='flex-1 flex flex-col p-4'>{children}</main>
+      <main className='flex-1 flex flex-col max-w-5xl mx-auto space-y-4 py-4'>
+        {children}
+      </main>
       <Footer />
     </div>
   );
